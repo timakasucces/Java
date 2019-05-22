3eb1a29c43c6736d1a13ac715c26d78d6e4ccb97
timakasucces <timakasucces@users.noreply.github.com>
2019-05-23 00:09:55 +0300

    clear

--- work2/src/AnimalClass.java
+++ work2/src/AnimalClass.java
@@ -1,11 +1,9 @@
 public abstract class AnimalClass implements Animal{
-    Boolean fedUp;
     String name;
     String food;
 
     AnimalClass(String name) {
         this.name = name;
-        this.fedUp = false;
     }
 
     @Override
